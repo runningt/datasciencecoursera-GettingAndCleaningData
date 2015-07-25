c40dcaba2853b4dc46186baafbefb266b855dc2e
Piotr Zawislak <running.t@gmail.com>
2015-07-26 01:56:26 +0200

    Assessment - first scratch

---
+++ assessment/run_analysis.R
@@ -0,0 +1,3 @@
+#download.file(url="https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", destfile="UCI HAR Dataset.zip", ,method="curl")
+#unzip("UCI HAR Dataset.zip")
+
